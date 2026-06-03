e0fb448ea66cdda12c652b9601362245058fc04d
Rxn321 <liuryan112255@gmail.com>
2026-06-03 11:56:41 -0700

    allowed vercel to access back

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://project-website-vercel-git-main-ryantyls-project1.vercel.app"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
